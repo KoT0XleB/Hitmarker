3c8eb3157d867f9acf720e7d5b9a0c135ad4aa60
KoT0XleB <lapka.roman@yandex.ru>
2022-07-12 13:46:35 +0500

    v1

---
+++ HitMarker/HitMarker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Qurre;
+using Qurre.API.Events;
+using Qurre.Events;
+
+namespace HitMarker
+{
+    public class HitMarker : Plugin
+    {
+        public override string Developer => "KoT0XleB#4663";
+        public override string Name => "Hitmarker";
+        public override Version Version => new Version(1, 0, 0);
+        public override int Priority => int.MinValue;
+        public override void Enable() => Player.DamageProcess += OnDamageProcess;
+        public override void Disable() => Player.DamageProcess -= OnDamageProcess;
+        public static void OnDamageProcess(DamageProcessEvent ev)
+        {
+            string color = "red";
+            if (ev.Attacker.Team != Team.SCP)
+            {
+                if (ev.Target.Ahp > 0) color = "42aaff";
+                else color = "ff0000";
+                ev.Attacker.ShowHint($"       <size=25><color=#{color}>X</color></size>  <size=25>[<color=#{color}>{(int)ev.Amount}</color>]\n\n\n\n\n\n\n\n\n\n\n", 2f);
+            }
+        }
+    }
+}
